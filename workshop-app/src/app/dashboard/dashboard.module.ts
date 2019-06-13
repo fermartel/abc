880251b253acfff9c093f3f126a59ff52c3630a9
fernando-a-martel <fernando.a.martel@pwc.com>
2019-06-13 10:17:16 -0400

    Add video list, player, and start filter dashboard.

--- workshop-app/src/app/dashboard/dashboard.module.ts
+++ workshop-app/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { StatFiltersComponent } from './stat-filters/stat-filters.component';
+
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    VideoDashboardComponent,
+    VideoListComponent,
+    VideoPlayerComponent,
+    StatFiltersComponent
+  ],
+  imports: [RouterModule.forChild(routes), CommonModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}

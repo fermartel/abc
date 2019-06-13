3786f0f1fbb843940fd161fb0d74c9fac00f0549
fernando-a-martel <fernando.a.martel@pwc.com>
2019-06-13 11:39:50 -0400

    DIsplay videos from json and add material css.

--- workshop-app/src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ workshop-app/src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-dashboard.component.css']
 })
 export class VideoDashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
